4fe1b29625a50c0301c163bc22a4e6549658dbc4
Samuel Wong <samuelw098@gmail.com>
2025-09-22 23:53:31 -0600

    added loaded plate component

--- src/lib/components/LoadedPlate.svelte
+++ src/lib/components/LoadedPlate.svelte
@@ -1,9 +1,15 @@
 <script>
 	// Plates on the barbell
-	// Circular plates you can click + counter
-	let { weight, color, size, removePlate } = $props();
+	// Rectangular plates you can click to remove
+	let { weight, color, size, formatLoadedPlateNumber, removePlate } = $props();
+	let width = $derived(size.width + 'px');
+	let height = $derived(size.height + 'px');
 </script>
 
-<button onclick={removePlate}>
-	<div class="w-2 h-5 bg-red-500">{weight}</div>
+<button
+	onclick={removePlate}
+	class={`cursor-pointer whitespace-pre-line rounded-sm mr-0.5 ${color} font-geist text-xs text-white font-medium`}
+	style="min-width: {width}; min-height: {height}"
+>
+	{formatLoadedPlateNumber(weight)}
 </button>
